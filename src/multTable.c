da178f6b698f9da66169f7cb302ad341a5c898d4
aryan <aryankhan8654272@gmail.com>
2023-10-22 01:53:08 -0700

    3rd Commit

--- src/multTable.c
+++ src/multTable.c
@@ -25,6 +25,10 @@ int main(int argc, char **argv) {
   for (int i = 1; i <= n; i++)
   {
     for (int j = 1; j <= n; j++)
-    
+    {
+      printf("%4d", i * j); 
+    }
+    printf("\n"); 
+  }
       return 0;
   }
