039004aeb8bc056455868e6a0526a105f00f1aa2
aryan <aryankhan8654272@gmail.com>
2023-10-22 00:37:19 -0700

    2nd Commit

--- src/exercises.c
+++ src/exercises.c
@@ -34,10 +34,9 @@ int main(int argc, char **argv) {
   printf("\n");
 
   //2. Rewrite the given while loop as an equivalent for loop
-  int k = 3;
-  while(k > -4) {
+  for (int k = 3; k > -4; k--)
+  {
     printf("%d ", k);
-    k--;
   }
   printf("\n");
 
